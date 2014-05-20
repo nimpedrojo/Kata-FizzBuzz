435751fe4f0faa297b6e1a0b682bec82674a5fe3
Pedro <pedro.lafuente@gmail.com>
2014-05-20 16:33:28 +0200

    Solucion sin refactorizar

---
+++ src/es/fizzbuzzkata/FizzBuzzKata.java
@@ -0,0 +1,38 @@
+package es.fizzbuzzkata;
+
+public class FizzBuzzKata {
+	public String generaResultados(int n){
+		String resultado = "";
+
+		for(int i = 1; i <= n; i++){
+			resultado += this.obtieneRespuesta(i) + "\n";
+		}
+
+		return resultado;
+	}
+
+	public String obtieneRespuesta(int i){
+		String respuesta = "";
+
+		if(i % 3 == 0 || this.contiene(i, "3"))
+			respuesta = "Fizz";
+
+		if(i % 5 == 0 || this.contiene(i, "5"))
+			respuesta += "Buzz";
+
+		if(respuesta.equals(""))
+			respuesta = String.valueOf(i);
+
+		return respuesta;
+	}
+
+	private boolean contiene(int i, String aguja) {
+		String iString = String.valueOf(i);
+
+		if(iString.contains(aguja))
+			return true;
+
+		return false;
+	}
+
+}
--- src/es/fizzbuzzkata/FizzBuzzKataTest.java
+++ src/es/fizzbuzzkata/FizzBuzzKataTest.java
@@ -1,14 +1,77 @@
 package es.fizzbuzzkata;
 
+
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzKataTest {
+	private FizzBuzzKata fb;
 
-	/*@Test
-	public void nombreExplicativoDelTest() {
-		assertEquals(Valoresperado,ValorObtenido);
-	}*/
+	@Before
+	public void setUp()
+	{
+		fb = new FizzBuzzKata();
+	}
 
+	@Test
+	public void testDevuelve1SiNumeroEs1() {
+		assertEquals("1", fb.obtieneRespuesta(1));
+	}
+
+	@Test
+	public void testDevuelve2SiNumeroEs2() {
+		assertEquals("2", fb.obtieneRespuesta(2));
+	}
+
+	@Test
+	public void testDevuelveFizzSiNumeroEs3() {
+		assertEquals("Fizz", fb.obtieneRespuesta(3));
+	}
+
+	@Test
+	public void testDevuelveBuzzSiNumeroEs5() {
+		assertEquals("Buzz", fb.obtieneRespuesta(5));
+	}
+
+	@Test
+	public void testDevuelveFizzSiNumeroEs6() {
+		assertEquals("Fizz", fb.obtieneRespuesta(6));
+	}
+
+	@Test
+	public void testDevuelveBuzzSiNumeroEs10() {
+		assertEquals("Buzz", fb.obtieneRespuesta(10));
+	}
+
+	@Test
+	public void testDevuelveFizzSiNumeroEs13() {
+		assertEquals("Fizz", fb.obtieneRespuesta(13));
+	}
+
+	@Test
+	public void testDevuelveFizzBuzzSiNumeroEs15() {
+		assertEquals("FizzBuzz", fb.obtieneRespuesta(15));
+	}
+
+	@Test
+	public void testDevuelveFizzSiNumeroEs18() {
+		assertEquals("Fizz", fb.obtieneRespuesta(18));
+	}
+
+	@Test
+	public void testDevuelveBuzzSiNumeroEs52() {
+		assertEquals("Buzz", fb.obtieneRespuesta(52));
+	}
+
+	@Test
+	public void testSiGeneraResultadoDe10() {
+		assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n", fb.generaResultados(10));
+	}
+
+	@Test
+	public void testSiGeneraResultadoDe13() {
+		assertEquals("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\nFizz\n", fb.generaResultados(13));
+	}
 }
