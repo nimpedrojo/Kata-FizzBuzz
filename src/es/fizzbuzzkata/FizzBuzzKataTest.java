cd42109faccbcbf5ea6123ec37c3e8aa04711116
Pedro <pedro.lafuente@gmail.com>
2014-05-20 16:12:29 +0200

    Añadiendo codigo

---
+++ src/es/fizzbuzzkata/FizzBuzzKataTest.java
@@ -0,0 +1,14 @@
+package es.fizzbuzzkata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzzKataTest {
+
+	/*@Test
+	public void nombreExplicativoDelTest() {
+		assertEquals(Valoresperado,ValorObtenido);
+	}*/
+
+}
